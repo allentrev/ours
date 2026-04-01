bc7accec90cd00aaf718726ed9c82ed3c94fd65b
allentrev <allentrev88@gmail.com>
2026-04-01 10:27:44 +0100

    write issue 3

--- backend/middlewares/auth.ts
+++ backend/middlewares/auth.ts
@@ -4,7 +4,9 @@ import User from "../models/user.model.js";
 import { UserDocument } from "../types/user.js";
 
 export const requireAuth = async (req: Request, res: Response, next: NextFunction) => {
+  console.log("req.auth:", req.auth);
   const clerkUserId = req.auth?.userId;
+  console.log("Clerk User ID:", clerkUserId);
   if (!clerkUserId) {
     return res.status(401).json("Not authenticated!");
   }
